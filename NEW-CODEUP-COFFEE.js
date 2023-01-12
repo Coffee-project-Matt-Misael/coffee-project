382da2bb9bd6c81e42cf48e081b8ba69c3fecad8
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-01-12 10:52:28 -0600

    almost final project

--- NEW-CODEUP-COFFEE.js
+++ NEW-CODEUP-COFFEE.js
@@ -2,21 +2,26 @@
 
 // COFFEE - TABLE CONTENT //
 
+let ligthRoast = "photos/light-roast.png"
+let mediumRoast = "photos/medium-roast.jpg"
+let darkRoast = "photos/dark-roast.jpg"
+
+
 const coffees = [
-    { id: 1, name: "Light City", roast: "light", rating: "&#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
-    { id: 2, name: "Half City", roast: "light" },
-    { id: 3, name: "Cinnamon", roast: "light" },
-    { id: 4, name: "City", roast: "medium" },
-    { id: 5, name: "American", roast: "medium" },
-    { id: 6, name: "Breakfast", roast: "medium" },
-    { id: 7, name: "High", roast: "dark" },
-    { id: 8, name: "Continental", roast: "dark" },
-    { id: 9, name: "New Orleans", roast: "dark" },
-    { id: 10, name: "European", roast: "dark" },
-    { id: 11, name: "Espresso", roast: "dark" },
-    { id: 12, name: "Viennese", roast: "dark" },
-    { id: 13, name: "Italian", roast: "dark" },
-    { id: 14, name: "French", roast: "dark" },
+    { id: 1, pic: ligthRoast, name: "Light City", roast: "light", rating: "&#11088 &#11088 &#11088 &#11088 &#11088",  description:  " A light coffee most popular in the big city" },
+    { id: 2, pic: ligthRoast, name: "Half City", roast: "light", rating: "&#11088 &#11088 &#11088",  description:  " Similar to 'light City' but more popular in smaller cities" },
+    { id: 3, pic: ligthRoast, name: "Cinnamon", roast: "light", rating: "&#11088 &#11088 &#11088 &#11088",  description:  "A true classic Cinnamon taste and fan favorite" },
+    { id: 4, pic: mediumRoast, name: "City", roast: "medium", rating: "&#11088",  description:  "This flavor is again similar to our 'light City' drink but has a darker roast" },
+    { id: 5, pic: mediumRoast, name: "American", roast: "medium", rating: "&#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 6, pic: mediumRoast, name: "Breakfast", roast: "medium", rating: "&#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 7, pic: darkRoast, name: "High", roast: "dark", rating: "&#11088 &#11088 &#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 8, pic: darkRoast, name: "Continental", roast: "dark", rating: "&#11088 &#11088 &#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 9, pic: darkRoast, name: "New Orleans", roast: "dark", rating: "&#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 10, pic: darkRoast, name: "European", roast: "dark", rating: "&#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 11, pic: darkRoast, name: "Espresso", roast: "dark", rating: "&#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 12, pic: darkRoast, name: "Viennese", roast: "dark", rating: "&#11088 &#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 13, pic: darkRoast, name: "Italian", roast: "dark", rating: "&#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
+    { id: 14, pic: darkRoast, name: "French", roast: "dark", rating: "&#11088 &#11088 &#11088",  description:  "A light coffee most popular in the big city" },
 ];
 
 const inputName = document.querySelector("#input-name");
@@ -26,6 +31,7 @@ const searchBox = document.querySelector('#searchBox');
 const roastSelect = document.querySelector('#roast-selection')
 addCoffeeButton.addEventListener("click", () => {
     addCoffees();
+
 });
 
 function addCoffees(input) {
@@ -48,6 +54,7 @@ function addCoffees(input) {
 // PUTS COFFEE DATA INTO TABLE WITHIN JS //
 function renderCoffee(coffee) {
     const html = `<div class="coffee d-block col-4 mx-auto p-auto">
+    <div class="coffee-case"><img class="coffee-pics" src="${coffee.pic}"></div>
     <h1>${coffee.name}</h1>
     <p class="roast">${coffee.roast}</p>
     <p class="rating">${coffee.rating}</p>
